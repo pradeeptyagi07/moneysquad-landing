5fbfd0f127201e6aa864b07759cfe1e89286184f
Tapan Upadhyay <tapanupa4321@gmail.com>
2025-05-21 16:32:54 +0530

    new chnagess

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1309,7 +1309,7 @@ const ContactPage = () => {
                 borderColor: "white",
                 "&:hover": {
                   borderColor: "white",
-                  backgroundColor: "rgba(255, 255, 255, 0.1)",
+                  backgroundColor: "rgba(255, 255, 255, 0.2)",
                 },
               }}
             >
